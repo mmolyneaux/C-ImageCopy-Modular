1f5bdaaa4f2c334d4d8518afafdeac4cdfe73243
mmolyneaux <m_molyneaux@hotmail.com>
2025-01-11 01:35:14 -0700

    Refactored readImage function

--- main.c
+++ main.c
@@ -2,65 +2,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #define CT_SIZE 1024 // Bitmap color table size
 #define HEADER_SIZE 54
 
-int main(int argc, char *argv[]) {
+typedef struct {
+    unsigned char header[HEADER_SIZE];
+    unsigned int width;
+    unsigned int height;
+    unsigned int bitDepth;
+    bool CT_EXISTS;
+    unsigned char colorTable[CT_SIZE];
+    unsigned char *imageBuffer;
+} bitmap;
 
-    FILE *streamIn = fopen("images/cameraman.bmp", "rb");
-    FILE *streamOut = fopen("images/cameraman_copy.bmp", "wb");
-
+void readImage(char *filename1, bitmap *bitmapIn) {
+    printf("readImage: %d\n", bitmapIn->width);
+    FILE *streamIn = fopen(filename1, "rb");
     if (streamIn == NULL) {
         printf("Error opening file!\n");
-    }
-
-    unsigned char header[HEADER_SIZE];
-    unsigned char colorTable[CT_SIZE];
+    } // TODO: need to return error from here.
 
     for (int i = 0; i < HEADER_SIZE; i++) {
-        header[i] = getc(streamIn);
+        bitmapIn->header[i] = getc(streamIn);
     }
+    // TODO: Make sure bitmap is initialized somewhere.
+    bitmapIn->width = *(int *)&bitmapIn->header[18];
+    bitmapIn->height = *(int *)&bitmapIn->header[22];
+    bitmapIn->bitDepth = *(int *)&bitmapIn->header[28];
+
+    const int IMAGE_SIZE = bitmapIn->width * bitmapIn->height;
+
+    bitmapIn->CT_EXISTS = false; // should already be initialized somewhere.
+    if (bitmapIn->bitDepth <=
+        8) { // by definition of bitmap, <= 8 has a color table
+        bitmapIn->CT_EXISTS = true;
+    }
+
+    if (bitmapIn->CT_EXISTS) {
+        fread(bitmapIn->colorTable, sizeof(char), CT_SIZE, streamIn);
+    }
+    bitmapIn->imageBuffer = (char *)malloc(IMAGE_SIZE);
+    // unsigned char buffer[IMAGE_SIZE];
+
+    fread(bitmapIn->imageBuffer, sizeof(char), IMAGE_SIZE, streamIn);
+    fclose(streamIn);
+}
+
+int main(int argc, char *argv[]) {
+
+    char *filename1 = NULL;
+    char *filename2 = NULL;
+
+    if (argc > 1) {
+        filename1 = argv[1];
+        printf("Filename1: %s\n", filename1);
+    }
+    if (argc > 2) {
+        filename2 = argv[2];
+        printf("Filename2: %s\n", filename2);
+    }
+    bitmap bitmapIn;
+    readImage(filename1, &bitmapIn);
 
     // width starts at address of byte(char) 18, which is then cast to an int*,
     // so it can be dereferenced into an int, so it is cast to a 4 byte int
     // instead stead of a single byte from the char header array. Then the
     // height can be retreived from the next 4 byts and so on.
-    unsigned int width = *(int *)&header[18];
-    unsigned int height = *(int *)&header[22];
-    unsigned int bitDepth = *(int *)&header[28];
-
-    const unsigned int IMAGE_SIZE = width * height;
 
     // if the bit depth is less than or equal to 8 then we need to read the
     // color table. The read content is going to be stored in colorTable. Not
-    // all bitmap images have color tables.    
-    bool CT_EXISTS = false;
-    if (bitDepth <= 8) {
-        CT_EXISTS = true;
+    // all bitmap images have color tables.
+
+    FILE *streamOut = fopen(filename2, "wb");
+    fwrite(bitmapIn.header, sizeof(char), HEADER_SIZE, streamOut);
+
+    if (bitmapIn.CT_EXISTS) {
+        fwrite(bitmapIn.colorTable, sizeof(char), CT_SIZE, streamOut);
     }
-
-    if (CT_EXISTS) {
-        fread(colorTable, sizeof(unsigned char), CT_SIZE, streamIn);
-    }
-
-    fwrite(header, sizeof(unsigned char), HEADER_SIZE, streamOut);
-
-    unsigned char buffer[IMAGE_SIZE];
-
-    fread(buffer, sizeof(unsigned char), IMAGE_SIZE, streamIn);
-
-    if (CT_EXISTS) {
-        fwrite(colorTable, sizeof(unsigned char), CT_SIZE, streamOut);
-    }
-    fwrite(buffer, sizeof(unsigned char), IMAGE_SIZE, streamOut);
+    unsigned int imageSize = bitmapIn.width * bitmapIn.height;
+    fwrite(bitmapIn.imageBuffer, sizeof(char), imageSize, streamOut);
     fclose(streamOut);
-    fclose(streamIn);
-    
-    
-    printf("width: %d\n", width);
-    printf("height: %d\n", height);
-    printf("bitDepth: %d\n", bitDepth);
 
+    printf("width: %d\n", bitmapIn.width);
+    printf("height: %d\n", bitmapIn.height);
+    printf("bitDepth: %d\n", bitmapIn.bitDepth);
+
+    // free(bitmapIn.imageBuffer);
     return 0;
 }
